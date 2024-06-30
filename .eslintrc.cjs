ea4fb24cb7a03b4f3f30da60e045c79d5cc71d50
zhengzc <zhengzc@servyou.com.cn>
2024-06-30 21:30:19 +0800

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:vue/essential', 'prettier', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/warnings',
+    'plugin:vue/essential',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
   rules: {
     'prettier/prettier': ['error', require('./.prettierrc.js')],
     'vue/multi-word-component-names': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off'
   }
 };
